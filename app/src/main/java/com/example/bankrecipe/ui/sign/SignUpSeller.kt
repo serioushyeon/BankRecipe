f61c88e67e6c50cd43cf6b037ff5fe37b64965aa
rdridrid <rdridrid@naver.com>
2023-06-18 18:01:17 +0900

    Sign Seller Activity

--- app/src/main/java/com/example/bankrecipe/ui/sign/SignUpSeller.kt
+++ app/src/main/java/com/example/bankrecipe/ui/sign/SignUpSeller.kt
@@ -2,11 +2,23 @@ package com.example.bankrecipe.ui.sign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.bankrecipe.R
+import com.example.bankrecipe.databinding.ActivitySelectSignUpTypeBinding
+import com.example.bankrecipe.databinding.ActivitySignUpSellerBinding
 
 class SignUpSeller : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpSellerBinding
+    private lateinit var BackBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_seller)
+        binding = ActivitySignUpSellerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        BackBtn = binding.SignUpSellerBackBtn
+
+        BackBtn.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
